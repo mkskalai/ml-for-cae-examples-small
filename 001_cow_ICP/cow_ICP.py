4cf5176538a822e80e3730b5b83b24b0c4d6c3cc
mkskalai <maksym.kalaidov@gmail.com>
2024-03-02 16:20:11 +0100

    add comments

--- 001_cow_ICP/cow_ICP.py
+++ 001_cow_ICP/cow_ICP.py
@@ -2,19 +2,25 @@ import pyvista as pv
 from pyvista import examples
 import imageio
 
+# download Cow mesh and position it more naturally for iso view
 source = examples.download_cow().triangulate()
 source = source.rotate_x(90)
 
+# save the initial camera position so that images do not jump in the gif
 pl = pv.Plotter(off_screen=True)
 pl.add_mesh(source)
 pl.show()
 camera = pl.camera_position
 
+# transform the source mesh and run PyVista's ICP to align it back
 transformed = source.rotate_y(-30).translate([-1.75, -0.5, 1.5])
 aligned = transformed.align(source)
 
+# Get the translations of each node
 translation = aligned.points - transformed.points
 
+# create 30 frames where transformed mesh is gradally 
+# moved back to it's original position
 screenshots = []
 num_iters = 30
 for i in range(num_iters):
